60c19f7346f8306247d4a496047dfc328bb6918b
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-05-13 16:34:57 +0530

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import Navbar from './components/Navbar'
+
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -19,6 +21,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forogt-password" element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
     </>
   )
