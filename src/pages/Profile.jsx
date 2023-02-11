a9c3ed5b1b910058e8b4898ff3707f99e5629a67
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2023-02-11 13:20:04 +0530

    User Sign In

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,16 @@
+import { useState, useEffect } from 'react'
+import { getAuth } from 'firebase/auth'
+
 const Profile = () => {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  )
+  const auth = getAuth()
+
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [auth])
+
+  return user ? <h1>{user.displayName}</h1> : 'Not Logged In'
 }
 
 export default Profile
