c0dd96f85f379040d75e9aa7337b0ce3d0072653
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2023-02-11 22:27:15 +0530

    Offer Page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,7 +1,84 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore'
+
+import { db } from '../firebase.config'
+import Spinner from '../components/Spinner'
+import ListingItem from '../components/ListingItem'
+
 const Offers = () => {
+  const { categoryName } = useParams()
+
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get reference
+        const listingsRef = collection(db, 'listings')
+
+        // Create a query
+        const q = query(
+          listingsRef,
+          where('offer', '==', true),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        )
+
+        // Execute query
+        const querySnap = await getDocs(q)
+
+        const lstings = []
+
+        querySnap.forEach((doc) =>
+          lstings.push({ id: doc.id, data: doc.data() })
+        )
+
+        setListings(lstings)
+        setLoading(false)
+      } catch (error) {
+        toast.error('Could not fetch listings')
+      }
+    }
+
+    fetchListings()
+  }, [])
+
   return (
-    <div>
-      <h1>Offers</h1>
+    <div className="category">
+      <header>
+        <p className="pageHeader">Offers</p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>There are no current offers</p>
+      )}
     </div>
   )
 }
